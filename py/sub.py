7f0928a531748b69e828f71b8b30d2d0c0deccf4
Ryo Komiyama <ryohei@rhizomatiks.com>
2018-01-17 20:11:25 +0900

    work properly

--- py/sub.py
+++ py/sub.py
@@ -3,28 +3,30 @@ import zmq
 import numpy as np
 import cv2
 
+print("Python: Camera subscriber!")
+    
 # Open ZMQ Connection
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://localhost:5555")
 socket.setsockopt(zmq.SUBSCRIBE, "")
-print("komi")
 
-# Receve Data from C++ Program
-data =  socket.recv_unicode()
-print("komi")
+while True:
+    # Receve Data from C++ Program
+    data =  socket.recv()
 
-# Convert byte to integer
-rows = 480
-cols = 640
-mat_type = 16
+    # Convert byte to integer
+    rows = 480
+    cols = 640
+    mat_type = 16
 
-if mat_type == 0:
-    # Gray Scale
-    image = np.frombuffer(data, dtype=np.uint8).reshape((rows,cols));
-else:
-    # BGR Color
-    image = np.frombuffer(data, dtype=np.uint8).reshape((rows,cols,3));
+    if mat_type == 0:
+        # Gray Scale
+        image = np.frombuffer(data, dtype=np.uint8).reshape((rows,cols));
+    else:
+        # BGR Color
+        image = np.frombuffer(data, dtype=np.uint8).reshape((rows,cols,3));
 
-# Write BMP Image
-cv2.imwrite("recv.bmp", image);
+    # Write BMP Image
+    cv2.imshow("Python Subscriber", image)
+    cv2.waitKey(10)
